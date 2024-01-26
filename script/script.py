2cd7ae273489d10941d7f9cfde8f181a56920923
Mannboisson <manon.boisson@icloud.com>
2024-01-26 08:01:44 +0100

    Modifications script

--- script/script.py
+++ script/script.py
@@ -4,14 +4,13 @@ import pandas as pd
 app = Flask(__name__)
 
 # Charger le fichier CSV provenant de Talend
-film_affiche_csv = "film_affiche_/_output/film_affiche.csv"
-df_talend = pd.read_csv(film_affiche_csv)
+film_affiche_csv = "../talend/_output/film_affiche.csv"
+df_talend = pd.read_csv(film_affiche_csv, sep=';')
 df_talend
 @app.route('/')
 def index():
     # Prendre les 5 premiers films du fichier CSV
-    cinq_premiers_films = df_talend.head(5)
-    print(cinq_premiers_films)
+    cinq_premiers_films = df_talend.tail(5)
 
     return render_template("index.html", films=cinq_premiers_films.to_dict(orient='records'))
 
