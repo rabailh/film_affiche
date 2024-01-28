287dac002113889babafdff21bba0255816244c3
Mannboisson <manon.boisson@icloud.com>
2024-01-28 20:34:53 +0100

    details films

--- script/script.py
+++ script/script.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
 import os, json
+import re
 
 # Obtenez le chemin absolu du répertoire actuel du script
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -14,9 +15,25 @@ df_films = pd.read_csv(film_affiche_csv, sep=';')
 # Convertir la colonne 'genres' en liste
 df_films['genres'] = df_films['genres'].apply(json.loads)
 
+# Fonction pour formater les noms des compagnies de production
+def format_production_companies(production_companies):
+    # Utilise une expression régulière pour extraire les éléments entre les guillemets
+    elements = re.findall(r'"([^"]*)"', production_companies)
+    # Joindre les éléments en une seule chaîne séparée par des virgules
+    result = ','.join(elements)
+    return result
+
+
 @app.route('/')
 def index():
     return render_template("index.html", films=df_films.to_dict(orient='records'))
+
+@app.route('/details/<film_id>')
+def details(film_id):
+    film = df_films[df_films['id'] == int(film_id)].iloc[0].to_dict()
+    # Formater les noms des compagnies de production
+    film['production_companies'] = format_production_companies(film['production_companies'])
+    return render_template("details.html", film=film)
 
 if __name__ == '__main__':
     app.run(debug=True)
